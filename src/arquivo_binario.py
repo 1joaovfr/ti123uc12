72abf90aec7aa348f28aa77d2af7af705fcdd1d1
rodrigosenacsp <rodrigo.mgsantos@senacsp.edu.br>
2024-09-05 11:15:06 -0300

    Update arquivo_binario.py

--- src/arquivo_binario.py
+++ src/arquivo_binario.py
@@ -7,17 +7,17 @@ class Pessoa:
         self.sobrenome = sobrenome
 
     def __str__(self):
-        return f"Pessoa(nome='{self.nome}',sobrenome='{self.sobrenome}')"
+        return f"Pessoa(nome='{self.nome}', sobrenome='{self.sobrenome}')"
 
-p = Pessoa("Rodrigo","Santos")
+p = Pessoa("Rodrigo", "Santos")
 print(p)
 
-file = open("pessoa.pkl", "wb")
-pickle.dump(p, file)
-file.close()
+# file = open("pessoa.pkl", "wb")
+# pickle.dump(p, file)
+# file.close()
 
-del p
+# del p
 
-with open("pessoa.pkl", "rb") as file:
-    p = pickle.load(file)
-    print(p)
+# with open("pessoa.pkl", "rb") as file:
+#     p = pickle.load(file)
+#     print(p)
